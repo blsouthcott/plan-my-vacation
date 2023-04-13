b84b58729761e1a3a374e0f53c980aefa6512678
Ben <benjamin.southcott@gmail.com>
2023-04-12 22:36:10 -0600

    remove option to edit plan

--- ui/src/components/vacationPlanResult.js
+++ ui/src/components/vacationPlanResult.js
@@ -1,25 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Pdf } from './pdf';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 
 
-export default function VacationPlanResult ({ plan, updatePlans }) {
-  const [isEditable, setIsEditable] = useState(false);
+export default function VacationPlanResult ({ plan }) {
   const navigate = useNavigate();
 
-  const handlePlanTextChange = (e) => {
-    const updatedPlan = {...plan};
-    updatedPlan.text = e.target.value;
-    updatePlans(updatedPlan);
-  }
-
-  const savePlan = () => {
-    setIsEditable(false);
-    // reload the page so the PDF will re-render with the changes the user made
-    // this is the only way I could get the changes to be reflected in the PDF
-    window.location.reload();
-  }
+  const pdfDownloadName = `${plan.location} Plan.pdf`;
 
   // takes the user back to the vacation plan form so they can generate another plan
   const startOver = () => {
@@ -37,19 +25,12 @@ export default function VacationPlanResult ({ plan, updatePlans }) {
         rows="15"
         cols="75"
         value={plan.text}
-        onChange={e => handlePlanTextChange(e)}
-        readOnly={!isEditable}
+        readOnly={true}
       />
       <div className="">
-        {isEditable ?
-          <button className="button is-primary is-light has-text-black is-outlined mt-3 mr-2 ml-2" onClick={savePlan}>Save Plan</button>
-        :
-          <button className="button is-primary is-light has-text-black is-outlined mt-3 mr-2 ml-2" onClick={() => setIsEditable(true)}>Edit Plan</button>
-        }
-        {/* &nbsp;&nbsp;&nbsp;&nbsp; */}
         {plan.text && plan.location &&
           <button className="button is-primary is-light has-text-black is-outlined mt-3 ml-2 mr-2">
-            <PDFDownloadLink document={<Pdf planText={plan.text} vacationLocation={plan.location}/>} fileName="vacation_plan.pdf">
+            <PDFDownloadLink document={<Pdf planText={plan.text} vacationLocation={plan.location}/>} fileName={pdfDownloadName}>
               Download as PDF
             </PDFDownloadLink>
           </button>}
